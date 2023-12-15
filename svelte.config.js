b8a29eb180aec353bd22d3f1e33feaa8c4221243
Yagnik Patel <coderisaddicted@gmail.com>
2023-12-15 22:43:11 +0530

    runtime changed

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: [vitePreprocess()],
 	kit: {
-		adapter: adapter({
-			runtime: "edge"
-		}),
+		adapter: adapter(),
 		alias: {
 			$: 'src'
 		},

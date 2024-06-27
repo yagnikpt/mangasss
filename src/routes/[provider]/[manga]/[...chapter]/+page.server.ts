458e6d3014a464798855d555537a6c9d89727581
Yagnik Patel <coderisaddicted@gmail.com>
2024-06-28 00:36:40 +0530

    error handing on chapter page

--- src/routes/[provider]/[manga]/[...chapter]/+page.server.ts
+++ src/routes/[provider]/[manga]/[...chapter]/+page.server.ts
@@ -1,35 +1,50 @@
 import type { PageServerLoad } from './$types';
 import { db } from '$/lib/server/db';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import type { LibraryRead } from '$/lib';
 
 export const load: PageServerLoad = async ({ locals, params }) => {
-	let reads: LibraryRead[] = [];
-	if (locals.user) {
-		const res = await db.query.user.findFirst({
-			columns: {
-				reads: true
-			},
-			where: (user, { eq }) => eq(user.id, locals.user?.id ?? '')
+	try {
+		let reads: LibraryRead[] = [];
+		if (locals.user) {
+			const res = await db.query.user.findFirst({
+				columns: {
+					reads: true
+				},
+				where: (user, { eq }) => eq(user.id, locals.user?.id ?? '')
+			});
+			if (res?.reads) reads = res.reads;
+		}
+		const { data } = await axios.get(`https://manga-server.vercel.app/meta/anilist-manga/read`, {
+			params: { chapterId: `${params.chapter}`, provider: params.provider }
 		});
-		if (res?.reads) reads = res.reads;
+		const {
+			data: { chapters }
+		} = await axios.get(`https://manga-server.vercel.app/meta/anilist-manga/info/${params.manga}`, {
+			params: { provider: params.provider }
+		});
+		const currentChpIndex = chapters.findIndex(
+			(chapter: any) => chapter.id === `${params.chapter}`
+		);
+		return {
+			pages: data || [],
+			mangaId: params.manga,
+			provider: params.provider,
+			chapter: params.chapter,
+			chapterTitle: chapters[currentChpIndex].title,
+			next: chapters[currentChpIndex + 1] ? chapters[currentChpIndex + 1] : null,
+			reads
+		};
+	} catch (error) {
+		if (error instanceof AxiosError) {
+			const errorObj: any = error.toJSON();
+			return {
+				error: error.message,
+				status: errorObj.status,
+				provider: params.provider,
+				id: params.manga
+			};
+		}
+		return { error: 'Server Error', status: 500, provider: params.provider, id: params.manga };
 	}
-	const { data } = await axios.get(`https://manga-server.vercel.app/meta/anilist-manga/read`, {
-		params: { chapterId: `${params.chapter}`, provider: params.provider }
-	});
-	const {
-		data: { chapters }
-	} = await axios.get(`https://manga-server.vercel.app/meta/anilist-manga/info/${params.manga}`, {
-		params: { provider: params.provider }
-	});
-	const currentChpIndex = chapters.findIndex((chapter: any) => chapter.id === `${params.chapter}`);
-	return {
-		pages: data || [],
-		mangaId: params.manga,
-		provider: params.provider,
-		chapter: params.chapter,
-		chapterTitle: chapters[currentChpIndex].title,
-		next: chapters[currentChpIndex + 1] ? chapters[currentChpIndex + 1] : null,
-		reads
-	};
 };
--- src/routes/[provider]/[manga]/[...chapter]/+page.svelte
+++ src/routes/[provider]/[manga]/[...chapter]/+page.svelte
@@ -9,12 +9,14 @@
 		GalleryHorizontal
 	} from 'lucide-svelte';
 	import type { PageData } from './$types';
-	import { refers } from '$/lib';
+	import { providers, refers } from '$/lib';
 	import * as Popover from '$lib/components/ui/popover';
 	import { Input } from '$/lib/components/ui/input';
 	import { cn } from '$/lib/utils';
 
 	export let data: PageData;
+
+	$: providersIds = providers.map((p) => p.value).filter((p) => p !== data.provider);
 
 	let currentPage = 1;
 	let popoverOpen = false;
@@ -25,18 +27,18 @@
 
 	async function syncToLocal() {
 		const read = {
-			id: data.mangaId,
-			provider: data.provider,
-			chapter: data.chapter,
+			id: data.mangaId!,
+			provider: data.provider!,
+			chapter: data.chapter!,
 			chapterTitle: data.chapterTitle ?? '',
 			page: currentPage
 		};
 		let lib = data.reads;
-		const currentReadIndex = lib.findIndex((read) => read.id === data.mangaId);
-		if (currentReadIndex === -1) lib.unshift(read);
+		const currentReadIndex = lib?.findIndex((read) => read.id === data.mangaId);
+		if (currentReadIndex === -1) lib?.unshift(read);
 		else {
-			lib = lib.filter((manga) => manga.id !== data.mangaId);
-			lib.unshift(read);
+			lib = lib?.filter((manga) => manga.id !== data.mangaId);
+			lib?.unshift(read);
 		}
 		await fetch('/api/sync', {
 			method: 'POST',
@@ -121,134 +123,150 @@
 
 <svelte:window on:keydown={handleKeyNavigation} />
 
-<div
-	class={`max-lg:fixed max-lg:top-0 max-lg:w-full max-lg:py-2 max-lg:px-4 max-lg:justify-between bg-neutral-900 border-b border-neutral-300 flex items-center lg:justify-around lg:h-[5dvh] z-10 ${
-		mode === 'vertical' ? 'fixed top-0 w-full py-2 px-4' : ''
-	}`}
-	class:max-lg:hidden={!showToolbar}
->
-	<span>{currentPage}/{data.pages.length}</span>
-	<div class="flex items-center gap-2">
-		<Popover.Root onOpenChange={(state) => (popoverOpen = state)}>
-			<Popover.Trigger class="p-2">
-				<BookOpenText />
-			</Popover.Trigger>
-			<Popover.Content align="end" class="w-60 space-y-4">
-				<span>Jump to a page</span>
-				<Input on:keydown={pageJump} class="p-2" min={1} max={data.pages.length} type="number" />
-			</Popover.Content>
-		</Popover.Root>
-		{#if mode === 'vertical'}
-			<button
-				on:click={() => {
-					mode = 'horizontal';
-					localStorage.setItem('read_mode', 'horizontal');
-				}}
-				aria-label="Read horizontal"
-				class="p-2"
-			>
-				<GalleryHorizontal />
-			</button>
-		{:else}
-			<button
-				on:click={() => {
-					mode = 'vertical';
-					localStorage.setItem('read_mode', 'vertical');
-				}}
-				class="p-2"
-				aria-label="Read verticle"
-			>
-				<GalleryVertical />
-			</button>
-		{/if}
-		<a
-			href={`/${data.provider}/${data.mangaId}`}
-			data-sveltekit-replacestate
-			aria-label="Close"
-			class="p-2 block"
-		>
-			<X />
-		</a>
+{#if data.error}
+	<div class="flex flex-col items-center gap-5 mt-12">
+		<p class="text-3xl font-medium">
+			Can't open manga from {data.provider.charAt(0).toUpperCase() + data.provider.slice(1)} :(
+		</p>
+		<p class="text-2xl">Try using different provider.</p>
+		<div class="flex gap-3">
+			{#each providersIds as provider}
+				<a class="block px-4 py-2 bg-neutral-800 rounded-md" href={`/${provider}/${data.id}`}
+					>{provider.charAt(0).toUpperCase() + provider.slice(1)}</a
+				>
+			{/each}
+		</div>
 	</div>
-</div>
-<div
-	class={`flex panel__container hide-scroll ${
-		mode === 'horizontal'
-			? 'h-[100dvh] lg:h-[90dvh] overflow-y-hidden overflow-x-scroll parent flex-row-reverse'
-			: 'flex-col overflow-x-hidden mt-20'
-	}`}
-	on:click={handleToolbarToggle}
-	role="presentation"
->
-	{#each data.pages as panel, index}
-		<div
-			data-page={index + 1}
-			class={cn(
-				'shrink-0 flex justify-center w-screen child items-center relative page',
-				mode === 'horizontal' && isPWA ? 'h-screen lg:h-[90dvh]' : 'h-[100dvh] lg:h-[90dvh]'
-			)}
-			id={`page-${index + 1}`}
-		>
+{:else}
+	<div
+		class={`max-lg:fixed max-lg:top-0 max-lg:w-full max-lg:py-2 max-lg:px-4 max-lg:justify-between bg-neutral-900 border-b border-neutral-300 flex items-center lg:justify-around lg:h-[5dvh] z-10 ${
+			mode === 'vertical' ? 'fixed top-0 w-full py-2 px-4' : ''
+		}`}
+		class:max-lg:hidden={!showToolbar}
+	>
+		<span>{currentPage}/{data.pages.length}</span>
+		<div class="flex items-center gap-2">
+			<Popover.Root onOpenChange={(state) => (popoverOpen = state)}>
+				<Popover.Trigger class="p-2">
+					<BookOpenText />
+				</Popover.Trigger>
+				<Popover.Content align="end" class="w-60 space-y-4">
+					<span>Jump to a page</span>
+					<Input on:keydown={pageJump} class="p-2" min={1} max={data.pages.length} type="number" />
+				</Popover.Content>
+			</Popover.Root>
+			{#if mode === 'vertical'}
+				<button
+					on:click={() => {
+						mode = 'horizontal';
+						localStorage.setItem('read_mode', 'horizontal');
+					}}
+					aria-label="Read horizontal"
+					class="p-2"
+				>
+					<GalleryHorizontal />
+				</button>
+			{:else}
+				<button
+					on:click={() => {
+						mode = 'vertical';
+						localStorage.setItem('read_mode', 'vertical');
+					}}
+					class="p-2"
+					aria-label="Read verticle"
+				>
+					<GalleryVertical />
+				</button>
+			{/if}
+			<a
+				href={`/${data.provider}/${data.mangaId}`}
+				data-sveltekit-replacestate
+				aria-label="Close"
+				class="p-2 block"
+			>
+				<X />
+			</a>
+		</div>
+	</div>
+	<div
+		class={`flex panel__container hide-scroll ${
+			mode === 'horizontal'
+				? 'h-[100dvh] lg:h-[90dvh] overflow-y-hidden overflow-x-scroll parent flex-row-reverse'
+				: 'flex-col overflow-x-hidden mt-20'
+		}`}
+		on:click={handleToolbarToggle}
+		role="presentation"
+	>
+		{#each data.pages as panel, index}
 			<div
-				class="w-12 h-12 border-4 lg:border-[6px] border-neutral-500 rounded-full border-t-current animate-spin text-neutral-950 absolute z-[-1]"
-			></div>
-			<img
-				class="lg:h-[90dvh] lg:w-auto w-[200vw] h-auto"
-				src={`https://m3u8-proxy-cors-alpha-two.vercel.app/cors?url=${
-					panel.img
-				}&headers={"referer":"${
-					panel.headerForImage ? panel.headerForImage.Referer : refers[data.provider]
-				}"}`}
-				alt={panel.page}
-				loading="lazy"
-				on:error={(e) => {
-					// @ts-ignore
-					e.currentTarget.src = `https://m3u8-proxy-cors-alpha-two.vercel.app/cors?url=${
+				data-page={index + 1}
+				class={cn(
+					'shrink-0 flex justify-center w-screen child items-center relative page',
+					mode === 'horizontal' && isPWA ? 'h-screen lg:h-[90dvh]' : 'h-[100dvh] lg:h-[90dvh]'
+				)}
+				id={`page-${index + 1}`}
+			>
+				<div
+					class="w-12 h-12 border-4 lg:border-[6px] border-neutral-500 rounded-full border-t-current animate-spin text-neutral-950 absolute z-[-1]"
+				></div>
+				<img
+					class="lg:h-[90dvh] lg:w-auto w-[200vw] h-auto"
+					src={`https://m3u8-proxy-cors-alpha-two.vercel.app/cors?url=${
 						panel.img
 					}&headers={"referer":"${
 						panel.headerForImage ? panel.headerForImage.Referer : refers[data.provider]
-					}"}`;
-				}}
-			/>
-		</div>
-	{/each}
-	{#if data.next}
-		<div
-			class="shrink-0 flex flex-col gap-4 justify-center w-screen h-[100dvh] child items-center end-page"
-		>
-			<p class="lg:text-lg">{data.next.title}</p>
-			<a
-				data-sveltekit-reload
-				href={`/${data.provider}/${data.mangaId}/${data.next.id}`}
-				class="block py-2 px-4 lg:text-lg bg-neutral-800 rounded-md">Read next chapter</a
+					}"}`}
+					alt={panel.page}
+					loading="lazy"
+					on:error={(e) => {
+						// @ts-ignore
+						e.currentTarget.src = `https://m3u8-proxy-cors-alpha-two.vercel.app/cors?url=${
+							panel.img
+						}&headers={"referer":"${
+							panel.headerForImage ? panel.headerForImage.Referer : refers[data.provider]
+						}"}`;
+					}}
+				/>
+			</div>
+		{/each}
+		{#if data.next}
+			<div
+				class="shrink-0 flex flex-col gap-4 justify-center w-screen h-[100dvh] child items-center end-page"
 			>
-		</div>
-	{:else}
-		<div
-			class="shrink-0 flex flex-col gap-4 justify-center w-screen h-[100dvh] child items-center end-page"
-		>
-			<p>That was the latest chapter.</p>
-			<a data-sveltekit-replacestate href={`/`} class="block py-2 px-4 bg-neutral-800 rounded-md"
-				>Home</a
+				<p class="lg:text-lg">{data.next.title}</p>
+				<a
+					data-sveltekit-reload
+					href={`/${data.provider}/${data.mangaId}/${data.next.id}`}
+					class="block py-2 px-4 lg:text-lg bg-neutral-800 rounded-md">Read next chapter</a
+				>
+			</div>
+		{:else}
+			<div
+				class="shrink-0 flex flex-col gap-4 justify-center w-screen h-[100dvh] child items-center end-page"
 			>
-		</div>
-	{/if}
-</div>
-<div
-	class="max-lg:hidden bg-neutral-900 border-t border-neutral-300 flex items-center lg:justify-around lg:h-[5dvh] z-10"
-	class:lg:hidden={mode === 'vertical'}
->
-	<button on:click={handleNextPage} class="p-2 flex">
-		<MoveLeft class="mr-2" /> Next
-	</button>
-	<button
-		disabled={currentPage === 1}
-		on:click={handlePrevPage}
-		class="p-2 flex items-center disabled:opacity-30"
+				<p>That was the latest chapter.</p>
+				<a data-sveltekit-replacestate href={`/`} class="block py-2 px-4 bg-neutral-800 rounded-md"
+					>Home</a
+				>
+			</div>
+		{/if}
+	</div>
+	<div
+		class="max-lg:hidden bg-neutral-900 border-t border-neutral-300 flex items-center lg:justify-around lg:h-[5dvh] z-10"
+		class:lg:hidden={mode === 'vertical'}
 	>
-		Prev <MoveRight class="ml-2" />
-	</button>
-</div>
+		<button on:click={handleNextPage} class="p-2 flex">
+			<MoveLeft class="mr-2" /> Next
+		</button>
+		<button
+			disabled={currentPage === 1}
+			on:click={handlePrevPage}
+			class="p-2 flex items-center disabled:opacity-30"
+		>
+			Prev <MoveRight class="ml-2" />
+		</button>
+	</div>
+{/if}
 
 <style>
 	.hide-scroll::-webkit-scrollbar {
